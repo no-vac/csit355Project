4700a88011ea8f8942909021a9980d11ab01f278
Sammy Samkough <sammysamkough@gmail.com>
2019-04-22 11:54:55 -0400

    Added all folders; the basis of some files (look at controllers); just some UX stuffs

--- public_html/index.php
+++ public_html/index.php
@@ -5,8 +5,10 @@
     <body>
         <?php require '../components/nav.php'; ?>
         <div class="container-fluid">
-            <div class="jumbotron m-4">
+            <div class="jumbotron m-4 text-center" style="background:url(../assets/light-brown-wood.jpg); background-position: center center">
                 <h2>Welcome to the 📱 Wallpapers Store!</h2>
+                <p>Here you'll find any and all premium mobile wallpapers!</p>
+                <p>Whether you want it to move or stop, our wallpapers are personal for you!</p>
             </div>
         </div>
         <?php require '../components/footer.php'; ?>
---
+++ server/dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+//This file has to be updated per local db user
+$host='localhost';
+$username='test';
+$password='test';
+$db='PhoneWallpapers';
+$mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error);
+?>
