7f60b98233a9f9dddfc2f8041cf2f14837cfacb0
Aadil Glasswala <aglasswala@gmail.com>
2019-04-30 18:22:23 -0400

    added cpanel db

--- server/dbConnection.php
+++ server/dbConnection.php
@@ -1,7 +1,7 @@
 <?php
-$host='130.68.20.108';
-$username='glassaad_local';
-$password='wallPapers';
-$db='glassaad_pwstore';
-$mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error);
+  $host='130.68.20.108';
+  $username='glassaad_local';
+  $password='wallPapers';
+  $db='glassaad_PWSTORE';
+  $mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error);
 ?>
