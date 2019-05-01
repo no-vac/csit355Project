fdd38e02886f408a0cbabb5fc6c98a73a711b9f6
Aadil Glasswala <aglasswala@gmail.com>
2019-04-30 23:47:06 -0400

    updating database

--- server/dbConnection.php
+++ server/dbConnection.php
@@ -1,7 +1,14 @@
 <?php
-  $host='130.68.20.108';
-  $username='glassaad_local';
-  $password='wallPapers';
-  $db='glassaad_PWSTORE';
-  $mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error);
+
+  // $host='130.68.20.108';
+  // $username='glassaad_home';
+  // $password='Wallpapers';
+  // $db='glassaad_PWSTORE';
+  // $mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error);
+
+  $host='localhost';
+  $username='test';
+  $password='test';
+  $db='pwstore';
+  $mysqli=new mysqli($host, $username, $password, $db) or die ($mysqli->error); 
 ?>
